335971f628c5ffe1f02a9544af5373fde6e81864
flarnhess <152910344+flarnhess@users.noreply.github.com>
2024-05-13 20:10:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+function myfunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+  function play() {
+    var audio = document.getElementById("audio");
+    audio.play();
+  }
+
+function degisbeniz() {
+  document.body.style.backgroundImage = 'url(assets/gbangi.png)';  
+}
+
+function tamekran() {
+  if ((document.fullScreenElement && document.fullScreenElement !== null) ||    
+   (!document.mozFullScreen && !document.webkitIsFullScreen)) {
+    if (document.documentElement.requestFullScreen) {  
+      document.documentElement.requestFullScreen();  
+    } else if (document.documentElement.mozRequestFullScreen) {  
+      document.documentElement.mozRequestFullScreen();  
+    } else if (document.documentElement.webkitRequestFullScreen) {  
+      document.documentElement.webkitRequestFullScreen(Element.ALLOW_KEYBOARD_INPUT);  
+    }  
+  } else {  
+    if (document.cancelFullScreen) {  
+      document.cancelFullScreen();  
+    } else if (document.mozCancelFullScreen) {  
+      document.mozCancelFullScreen();  
+    } else if (document.webkitCancelFullScreen) {  
+      document.webkitCancelFullScreen();  
+    }  
+  }  
+}
